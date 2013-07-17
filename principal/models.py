5615ec783da8f0f6741e54e068c651924dea9a61
xyiyox <yiyo1224@hotmail.com>
2013-07-17 13:11:02 -0500

    Recetario despues del capitulo:'El Modelo de Datos'

--- principal/models.py
+++ principal/models.py
@@ -1,4 +1,6 @@
+#encoding:utf-8
 from django.db import models
+from django.contrib.auth.models import User
 
 class Bebida(models.Model):
 	nombre = models.CharField(max_length=50)
@@ -7,4 +9,21 @@ class Bebida(models.Model):
 
 	def __unicode__(self):
 		return self.nombre
+		
+class Receta(models.Model):
+	#Dato cadena, longitud máxima 100 y único
+	titulo = models.CharField(max_length=100, unique=True)
+	#Dato texto, con texto de ayuda
+	ingredientes = models.TextField(help_text='Redacta los ingredientes')
+	#Dato texto, con nombre: Preparación
+	preparacion = models.TextField(verbose_name='Preparacion')
+	#Dato imagen, se almacenarán en la carpeta recetas, titulo: Imágen
+	imagen = models.ImageField(upload_to='recetas', verbose_name='Imagen')
+	#Dato Fecha y Hora, almacena la fecha actual
+	tiempo_registro = models.DateTimeField(auto_now=True)
+	#Enlace al modelo Usuario que Django ya tiene construido
+	usuario = models.ForeignKey(User)
+
+	def __unicode__(self):
+		return self.titulo
 		
--- principal/views.py
+++ principal/views.py
@@ -1,6 +1,11 @@
 from principal.models import Bebida
+from principal.models import Receta
 from django.shortcuts import render_to_response
 
 def lista_bebidas(request):
 	bebidas = Bebida.objects.all()
 	return render_to_response('lista_bebidas.html',{'lista':bebidas})
+
+def lista_recetas(request):
+	recetas = Receta.objects.all()
+	return render_to_response('lista_bebidas.html',{'lista_recetas':recetas})
